3a0cf03ffda7a95d9c1769966c6c7ad2c4839de4
PedroPerez <frontback.net@gmail.com>
2021-12-21 12:18:28 -0300

    actualizando archivo GifGridItem

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const GifGridItem = ({id, title, url}) => {
 
-    console.log({id, title, url});
-
     return (
         <div className="card animate__animated animate__fadeInUp">
             <img src={url} alt={title}/>
